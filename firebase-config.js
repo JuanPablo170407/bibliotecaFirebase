54a054e06638358cd4caea8c426535802975be25
JuanPablo170407 <pablo.lopezsanchez174@gmail.com>
2025-06-05 15:22:58 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
  const firebaseConfig = {
-    apiKey: "AIzaSyBTU",
-    authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "b19",
-    storageBucket: "bdnoapp",
-    messagingSenderId: "519",
-    appId: "1:5193611",
-    measurementId: "G0"
+   
+    
   };
 
 const app = initializeApp(firebaseConfig);
